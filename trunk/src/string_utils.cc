de514ab6bc0cbdc5cea59b2ee2eb5851f32c108e
vrok <vrok@b23a0f8f-adf1-0310-838a-fb65cca2307f>
2006-01-04 09:43:50 +0000

    Updated Copyright dates.

--- trunk/src/string_utils.cc
+++ trunk/src/string_utils.cc
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2005 Marcin R. Wrochniak <wrochniak@gmail.com>
+  Copyright (C) 2005-2006 Marcin R. Wrochniak <wrochniak@gmail.com>
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License, version 2 as
